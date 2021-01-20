0cbb13cdb867541570dfe732127b186a22a0a5d9
edenave <76529301+edenave@users.noreply.github.com>
2021-01-20 16:24:02 +0200

    q22 finished

---
+++ q22.js
@@ -0,0 +1,23 @@
+// function run() {
+//     const input = document.getElementById("input").value;
+//     const output = document.getElementById("output");
+//     let out = "";
+
+/**/
+    // write your code here
+    let arr1 = [12, 43, 45, 55];
+    let arr2 = [132, 44, 55];
+    for (var i=0; i < arr2.length; i++) {
+
+        arr1.push( arr2[i] );
+        
+
+        
+    }
+
+
+    console.log(arr1);
+/**/
+//     output.innerText = out;
+
+
